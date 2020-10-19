163c2a388671f7ff241a83219e72ebb93d1912b1
amir-kalantar <amir.kalantar74@gmail.com>
2020-10-19 13:13:57 +0330

    initial commit

---
+++ src/Rules/CheckRule.php
@@ -0,0 +1,9 @@
+<?php
+namespace Karamel\Validation\Rules;
+class CheckRule {
+    public static function check($key,$data,$name,$value)
+    {
+        $name = ucfirst($name);
+
+    }
+}
---
+++ src/Validation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Karamel\Validation;
+class Validation
+{
+
+    public static function make($data = [], $roles = [])
+    {
+        foreach ($roles as $key => $role) {
+            $role_row = explode("|", $role);
+            foreach ($role_row as $role_single) {
+                $role_single = explode(":", $role_single);
+                if (count($role_single) > 1) {
+                    $role_name = $role_single[0];
+                    $role_value = $role_single[1];
+                } else {
+                    $role_name = $role_single[0];
+                    $role_value = null;
+
+                }
+            }
+        }
+    }
+}
